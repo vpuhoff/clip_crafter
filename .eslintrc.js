887ccfb31a4313d9eece7e84ddabeb86e8c5ccb3
bad.robot <vpuhoff92@gmail.com>
2025-08-14 23:36:28 +0400

    Add development Makefile and lint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true }
+  }
+};
